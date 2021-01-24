b00f819febe730a9d86e5ad36d501b6749c049b6
Zach Stearman <zstearman3@gmail.com>
2021-01-24 19:00:28 +0000

    Add initial processing

--- rosters.py
+++ rosters.py
@@ -1,5 +1,10 @@
 from scraper.client import Client
+from scraper.processor import Processor
 
 client = Client()
 rosters = client.rosters()
-print(rosters)
+processor = Processor()
+for id, roster in rosters.items():
+  roster = processor.format_roster(roster)
+  # TODO: Add roster to db
+print(roster)
---
+++ scraper/processor.py
@@ -0,0 +1,9 @@
+class Processor:
+  def format_roster(self, roster):
+    formatted_roster = []
+    for player in roster:
+      name = player["name"].split(" ", 1)
+      player["first_name"] = name[0]
+      player["last_name"] = name[1]
+      formatted_roster.append(player)
+    return formatted_roster
