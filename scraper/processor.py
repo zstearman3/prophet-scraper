03aecdf440a6d10537cf40cb95369960c6444572
Zach Stearman <zstearman3@gmail.com>
2021-01-24 19:51:30 +0000

    Add height and weight processing

--- scraper/processor.py
+++ scraper/processor.py
@@ -3,7 +3,12 @@ class Processor:
     formatted_roster = []
     for player in roster:
       name = player["name"].split(" ", 1)
+      height = player["height"].split("'")
+      weight = player["weight"].split(" ")
+      height = (int(height[0]) * 12) + int(height[1][:-1])
       player["first_name"] = name[0]
       player["last_name"] = name[1]
+      player["height"] = height
+      player["weight"] = int(weight[0]) if weight[0].isnumeric() else None
       formatted_roster.append(player)
     return formatted_roster
