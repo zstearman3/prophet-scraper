a2e8c3ebe4cade7b4e4d316a0769504e05ae83b2
Zach Stearman <zstearman3@gmail.com>
2021-01-25 01:33:50 +0000

    Make height and weight conversions module functions

--- scraper/processor.py
+++ scraper/processor.py
@@ -1,14 +1,24 @@
+def convert_height(height):
+  try:
+    height = height.split("'")
+    height = (int(height[0]) * 12) + int(height[1][:-1])
+    return height
+  except:
+    return None
+
+def convert_weight(weight):
+  weight = weight.split(" ")
+  weight = int(weight[0]) if weight[0].isnumeric() else None
+  return weight
+
 class Processor:
   def format_roster(self, roster):
     formatted_roster = []
     for player in roster:
       name = player["name"].split(" ", 1)
-      height = player["height"].split("'")
-      weight = player["weight"].split(" ")
-      height = (int(height[0]) * 12) + int(height[1][:-1])
       player["first_name"] = name[0]
       player["last_name"] = name[1]
-      player["height"] = height
-      player["weight"] = int(weight[0]) if weight[0].isnumeric() else None
+      player["height"] = convert_height(player["height"])
+      player["weight"] = convert_weight(player["weight"])
       formatted_roster.append(player)
     return formatted_roster
